c52a4661df3a89059bb1587858e550675e7b3c06
shunti <sunnyhorse.minecraft@gmail.com>
2026-06-07 01:43:46 +0900

    Fix: Loom 1.14.10, LiteralText, command v1, player.sendChatMessage

--- src/main/java/com/minewordle/MineWordleMod.java
+++ src/main/java/com/minewordle/MineWordleMod.java
@@ -1,20 +1,18 @@
 package com.minewordle;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.minecraft.client.MinecraftClient;
 
 public class MineWordleMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
-            dispatcher.register(ClientCommandManager.literal("wordle")
+        ClientCommandManager.DISPATCHER.register(
+            ClientCommandManager.literal("wordle")
                 .executes(ctx -> openScreen(ctx.getSource().getClient(), false))
                 .then(ClientCommandManager.literal("practice")
                     .executes(ctx -> openScreen(ctx.getSource().getClient(), true)))
-            )
         );
     }
 
